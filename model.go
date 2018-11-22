729efc8883d9b5379f83bef1bb5e1f03326ac4cd
brodyaga <cybervagabound2@protonmail.com>
2018-11-22 13:16:15 -0500

    db methods created

--- model.go
+++ model.go
@@ -1,5 +1,32 @@
-type User struct {
+package model
+
+import (
+	"database/sql"
+	"github.com/pkg/errors"
+)
+
+type user struct {
 	UUID        int	   `json:"uuid"`
 	Username    string `json:"username"`
 	Registered  string `json:"registered"`
+}
+
+func (u *user) getUser(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (u *user) updateUser(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (u *user) deleteUser(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (u *user) createUser(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func getUsers(db *sql.DB, start, count int) ([]user, error) {
+	return nil, errors.New("Not implemented")
 }
