9d08bb54b8111724f09e302e457867a668e839fa
brodyaga <cybervagabound2@protonmail.com>
2018-11-23 02:01:42 -0500

    TestEmptyTable created

--- app.go
+++ app.go
@@ -16,11 +16,13 @@ type App struct {
 
 func (a *App) Initialize(user, password, dbname string) {
 	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+
 	var err error
 	a.DB, err = sql.Open("postgres", connectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	a.Router = mux.NewRouter()
 }
 
--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main_test
 
 import (
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 	"rpc_server"
@@ -43,3 +45,44 @@ username TEXT NOT NULL,
 registered TIMESTAMP NOT NULL DEFAULT NOW(),
 CONSTRAINT users_pkey PRIMARY KEY (id)
 )`
+
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/users", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
