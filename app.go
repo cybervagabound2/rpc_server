8fd7d76c11bcddf0bb34bef5f0c3db5a6c9f20b2
brodyaga <cybervagabound2@protonmail.com>
2018-11-23 04:33:37 -0500

    initializeRoutes for handle server requests

--- app.go
+++ app.go
@@ -31,6 +31,14 @@ func (a *App) Initialize(user, password, dbname string) {
 
 func (a *App) Run(addr string) {}
 
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
+	a.Router.HandleFunc("/user", a.createUser).Methods("POST")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.getUser).Methods("GET")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.updateUser).Methods("PUT")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.deleteUser).Methods("DELETE")
+}
+
 func (a *App) getUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
