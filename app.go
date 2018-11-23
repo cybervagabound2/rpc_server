def9bfb60ea1fea228f9b680f33f69862463796e
brodyaga <cybervagabound2@protonmail.com>
2018-11-23 04:35:23 -0500

    appRun

--- app.go
+++ app.go
@@ -27,9 +27,12 @@ func (a *App) Initialize(user, password, dbname string) {
 	}
 
 	a.Router = mux.NewRouter()
+	a.initializeRoutes()
 }
 
-func (a *App) Run(addr string) {}
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":8000", a.Router))
+}
 
 func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
