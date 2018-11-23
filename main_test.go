c8edaccdcc3acde39ea6de28f19447f40bf7fa38
brodyaga <cybervagabound2@protonmail.com>
2018-11-22 21:35:40 -0500

    Initialize DB for tests

---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"."
+)
+
+var a main.App
+
+func TestMain(m *testing.M) {
+	a = main.App{}
+	a.Initialize(
+		os.Getenv("TEST_DB_USERNAME"),
+		os.Getenv("TEST_DB_PASSWORD"),
+		os.Getenv("TEST_DB_NAME"))
+
+	ensureTableExists()
+
+	code := m.Run()
+
+	clearTable()
+
+	os.Exit(code)
+}
+
+func ensureTableExists() {
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS users_test
+(
+uuid VARCHAR(36),
+username TEXT NOT NULL,
+registered TIMESTAMP NOT NULL DEFAULT NOW(),
+CONSTRAINT users_pkey PRIMARY KEY (id)
+)`
--- model.go
+++ model.go
@@ -1,8 +1,11 @@
 package model
 
 import (
+	"crypto/rand"
 	"database/sql"
+	"fmt"
 	"github.com/pkg/errors"
+	"log"
 )
 
 type user struct {
@@ -23,10 +26,22 @@ func (u *user) deleteUser(db *sql.DB) error {
 	return errors.New("Not implemented")
 }
 
-func (u *user) createUser(db *sql.DB) error {
+func (u *user) addUser(db *sql.DB) error {
 	return errors.New("Not implemented")
 }
 
 func getUsers(db *sql.DB, start, count int) ([]user, error) {
 	return nil, errors.New("Not implemented")
+}
+
+// function to generate uuid for user
+func (u *user) genUuid(db *sql.DB) string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
+		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return uuid
 }
